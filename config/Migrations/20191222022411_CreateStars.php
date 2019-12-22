feabdf1c9e0c6c22cc2fb2c06b877fd223da9823
namida-neet <namida.neet@gmail.com>
2019-12-22 11:26:30 +0900

    マイグレーションファイル作成と実行

---
+++ config/Migrations/20191222022411_CreateStars.php
@@ -0,0 +1,41 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateStars extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('stars');
+        $table->addColumn('user_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('post_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('star_score', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
