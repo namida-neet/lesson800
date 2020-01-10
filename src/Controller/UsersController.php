c34610bd519727eb29c05a244e155ad986366719
namida-neet <namida.neet@gmail.com>
2020-01-10 20:19:12 +0900

    不要なアクションを削除した

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -41,7 +41,6 @@ class UsersController extends AppController
     {
         $this->request->session()->destroy();
         return $this->redirect($this->Auth->logout());
-
     }
 
     public function signup()
@@ -55,9 +54,9 @@ class UsersController extends AppController
 
                 // アイコン画像をアップロード
                 if (! empty($this->request->data['icon']['tmp_name'])) {
-                    $dir = realpath(WWW_ROOT . "/img/user-icon");
                     $tmpName = $this->request->data['icon']['tmp_name'];
-                    $iconFileName = $image = date('YmdHis') . $this->request->data['icon']['name'];
+                    $dir = realpath(WWW_ROOT . "/img/user-icon");
+                    $iconFileName = date('YmdHis') . $this->request->data['icon']['name'];
 
                     move_uploaded_file($tmpName, $dir . '/' . $iconFileName);
                 } else {
@@ -86,97 +85,5 @@ class UsersController extends AppController
             }
         }
         $this->set(compact('user'));
-    }
-
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
-    public function index()
-    {
-        $users = $this->paginate($this->Users);
-
-        $this->set(compact('users'));
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $user = $this->Users->get($id, [
-            'contain' => ['Favorites', 'Posts', 'Stars'],
-        ]);
-
-        $this->set('user', $user);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
-        }
-        $this->set(compact('user'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $user = $this->Users->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
-        }
-        $this->set(compact('user'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
-        } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
